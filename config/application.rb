9bf6d9b2b3663c7e88eef785f186ed21f442bff3
nhithutran <nhithutran@gmail.com>
2022-06-09 14:21:26 +1000

    add assets.initialize_on_prempile = false into application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Mypensilvania
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    #Add precompile assets config to try to solve push heroku error.
+    config.assets.initialize_on_precompile = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
